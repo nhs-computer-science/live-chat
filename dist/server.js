d6716459875098a7e6368a646096c0b53a3be8c8
alexkhazzam <akhazzam1@student.gn.k12.ny.us>
2021-05-22 15:41:42 -0400

    Adding

--- dist/server.js
+++ dist/server.js
@@ -1,17 +1,15 @@
-'use strict';
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
-Object.defineProperty(exports, '__esModule', { value: true });
-const dotenv_1 = __importDefault(require('dotenv'));
-const path_1 = __importDefault(require('path'));
-const listener_1 = __importDefault(require('./util/listener'));
-const app_1 = __importDefault(require('./app'));
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
+const path_1 = __importDefault(require("path"));
+const listener_1 = __importDefault(require("./util/listener"));
+const app_1 = __importDefault(require("./app"));
 dotenv_1.default.config({ path: path_1.default.join(__dirname, '../.env') });
-const DEFAULT_PORT = 5000;
-const FALLBACK_PORT = 3000;
+const DEFAULT_PORT = process.env.DEFAULT_PORT;
+const FALLBACK_PORT = process.env.FALLBACK_PORT;
 app_1.default.listen(DEFAULT_PORT || FALLBACK_PORT, () => {
-  listener_1.default(DEFAULT_PORT || FALLBACK_PORT);
+    listener_1.default(DEFAULT_PORT || FALLBACK_PORT);
 });
