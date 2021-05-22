0ceb0e497824c131540e04e59d2c9379a27cf12e
alexkhazzam <akhazzam1@student.gn.k12.ny.us>
2021-05-22 17:32:03 -0400

    adding

--- server.js
+++ server.js
@@ -5,12 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
 const path_1 = __importDefault(require("path"));
-const listener_1 = __importDefault(require("./util/listener"));
 const app_1 = __importDefault(require("./app"));
 dotenv_1.default.config({ path: path_1.default.join(__dirname, '.env') });
-let DEFAULT_PORT = 5000;
-let FALLBACK_PORT = 3000;
-console.log(DEFAULT_PORT, FALLBACK_PORT);
+let DEFAULT_PORT = process.env.DEFAULT_PORT;
+let FALLBACK_PORT = process.env.FALLBACK_PORT;
 app_1.default.listen(DEFAULT_PORT || FALLBACK_PORT, () => {
-    listener_1.default(DEFAULT_PORT || FALLBACK_PORT);
+    console.log('listening to request on port ' + DEFAULT_PORT || FALLBACK_PORT);
 });
--- server.ts
+++ server.ts
@@ -6,11 +6,9 @@ import app from './app';
 
 dotenv.config({ path: path.join(__dirname, '.env')});
 
-let DEFAULT_PORT: number | string = 5000;
-let FALLBACK_PORT: number | string = 3000;
+let DEFAULT_PORT: number | string = process.env.DEFAULT_PORT!
+let FALLBACK_PORT: number | string = process.env.FALLBACK_PORT!
 
-console.log(DEFAULT_PORT, FALLBACK_PORT);
-
-app.listen(DEFAULT_PORT || FALLBACK_PORT, (): void => {
-    listener(DEFAULT_PORT || FALLBACK_PORT);
+app.listen(DEFAULT_PORT || FALLBACK_PORT, () => {
+    console.log('listening to request on port ' + DEFAULT_PORT || FALLBACK_PORT);
 });
