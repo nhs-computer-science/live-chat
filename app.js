719bd3e4fe3d6d70b18e63f3beb1ea95c590a047
alexkhazzam <akhazzam1@student.gn.k12.ny.us>
2021-05-29 10:49:50 -0400

    DSfdsfddfddfs

--- app.js
+++ app.js
@@ -7,36 +7,47 @@ const express_1 = __importDefault(require("express"));
 const connect_mongo_1 = __importDefault(require("connect-mongo"));
 const express_session_1 = __importDefault(require("express-session"));
 const body_parser_1 = __importDefault(require("body-parser"));
-const mongodb_1 = __importDefault(require("./database/mongodb"));
+const mongoose_1 = __importDefault(require("mongoose"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const path_1 = __importDefault(require("path"));
+// import authenticateSession from './middleware/authenticateSession';
 const attendance_1 = __importDefault(require("./routes/attendance"));
 const attendanceToken_1 = __importDefault(require("./routes/attendanceToken"));
 const register_1 = __importDefault(require("./routes/register"));
-const authenticateSession_1 = __importDefault(require("./middleware/authenticateSession"));
 const app = express_1.default();
+// db.connect();
 dotenv_1.default.config({ path: path_1.default.join(__dirname, './.env') });
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 app.use(express_1.default.static(path_1.default.join(__dirname, './public')));
 app.use(body_parser_1.default.urlencoded({ extended: false }));
+const clientP = mongoose_1.default
+    .connect('mongodb+srv://admin-alex:xs5l99f2NdiAlTL1@nhs-computer-science-li.ncb4w.mongodb.net/nhs-computer-science-live-chat-db?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then((m) => {
+    console.log('connection established');
+    return m.connection.getClient();
+});
 app.use(express_session_1.default({
     secret: 'foo',
     resave: false,
     saveUninitialized: false,
     store: connect_mongo_1.default.create({
-        clientPromise: mongodb_1.default(),
-        dbName: process.env.MONGO_DB_DB,
+        clientPromise: clientP,
+        dbName: 'nhs-computer-science-live-chat-db',
         stringify: false,
-        autoRemove: 'disabled',
+        autoRemove: 'interval',
+        autoRemoveInterval: 1,
     }),
 }));
 app.get('/', (req, res, next) => {
-    req.session.m = Math.random().toString();
+    req.session.foo = 'sfd';
     res.send('dfsf');
 });
 app.use('/attendance', attendance_1.default);
 app.use('/register', register_1.default);
 app.use('/attendance-token', attendanceToken_1.default);
-app.use('/', authenticateSession_1.default);
+// app.use('/', authenticateSession);
 exports.default = app;
--- app.ts
+++ app.ts
@@ -2,17 +2,21 @@ import Express from 'express';
 import MongoStore from 'connect-mongo';
 import session from 'express-session';
 import bodyParser from 'body-parser';
+import cookieSession from 'cookie-session';
 import db from './database/mongodb';
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import path from 'path';
+
+// import authenticateSession from './middleware/authenticateSession';
 
 import attendanceRoute from './routes/attendance';
 import attendanceTokenRoute from './routes/attendanceToken';
 import registerRoute from './routes/register';
 
-import authenticateSession from './middleware/authenticateSession';
-
 const app = Express();
+
+// db.connect();
 
 dotenv.config({ path: path.join(__dirname, './.env') });
 
@@ -21,22 +25,36 @@ app.set('views', 'views');
 
 app.use(Express.static(path.join(__dirname, './public')));
 app.use(bodyParser.urlencoded({ extended: false }));
+const clientP = mongoose
+  .connect(
+    'mongodb+srv://admin-alex:xs5l99f2NdiAlTL1@nhs-computer-science-li.ncb4w.mongodb.net/nhs-computer-science-live-chat-db?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then((m) => {
+    console.log('connection established');
+    return m.connection.getClient();
+  });
+
 app.use(
   session({
     secret: 'foo',
     resave: false,
     saveUninitialized: false,
     store: MongoStore.create({
-      clientPromise: db(),
-      dbName: process.env.MONGO_DB_DB!,
+      clientPromise: clientP,
+      dbName: 'nhs-computer-science-live-chat-db',
       stringify: false,
-      autoRemove: 'disabled',
+      autoRemove: 'interval',
+      autoRemoveInterval: 1,
     }),
   })
 );
 
 app.get('/', (req, res, next) => {
-  req.session.m = Math.random().toString();
+  req.session.foo = 'sfd';
   res.send('dfsf');
 });
 
@@ -44,6 +62,6 @@ app.use('/attendance', attendanceRoute);
 app.use('/register', registerRoute);
 app.use('/attendance-token', attendanceTokenRoute);
 
-app.use('/', authenticateSession);
+// app.use('/', authenticateSession);
 
 export default app;
