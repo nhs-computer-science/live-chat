93503a42757a2bc661888969ce289d14a8aa0bde
alexkhazzam <akhazzam1@student.gn.k12.ny.us>
2021-06-06 20:19:12 -0400

    adding

--- public/js/login.ts
+++
@@ -1,8 +0,0 @@
-const email = document.getElementById('email')!;
-const loginBtn = document.getElementById('login-btn')!;
-
-email.value = window.sessionStorage.getItem('email') || '';
-
-loginBtn.addEventListener('click', (): void => {
-  window.sessionStorage.setItem('email', email.value.trim());
-});
